800262364026674fed8e6e97f65b8efb0eeed694
jswjia@163.com <jsw6295456>
2016-08-02 17:33:06 +0800

    初始化结构

---
+++ WebSocketMvcUI/BLL/BLLBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSocketMvcUI.BLL
+{
+    public class BLLBase
+    {
+    }
+}
---
+++ WebSocketMvcUI/Controllers/TransController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSocketMvcUI.Controllers
+{
+    public class TransController : Controller
+    {
+        //
+        // GET: /Trans/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetOrder(string Id)
+        { 
+            JsonResult result=new JsonResult();
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;//允许使用GET方式获取，否则用GET获取是会报错
+            result.Data = "[{name:" + Id + ",sex:男},{name:小芳,sex:女}]";
+            return result;
+        }
+
+    }
+}
